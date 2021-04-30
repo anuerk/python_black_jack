22053f555899a4719783cfccd0eb127b90a62dfc
portolanIT <it@portolancs.com>
2021-04-30 17:12:52 +0200

    fixed init

--- game_sources/__init__.py
+++ game_sources/__init__.py
@@ -6,6 +6,6 @@ modules that define `Card`, `Deck`, `Round` and `Player`
 
 from game_sources.cards import Card
 from game_sources.decks import Deck
-from game_sources.games import Game
 from game_sources.players import Player
 from game_sources.rounds import Round
+from game_sources.games import Game
